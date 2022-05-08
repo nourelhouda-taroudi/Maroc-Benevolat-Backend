b4d38c395d41713613ac30c54aee44b192a43d02
Oumaima Mt <oumaimamt@MacBook-Pro-de-Oumaima.local>
2022-05-08 00:07:12 +0000

    add database

---
+++ src/association/controllers/association.controller.ts
@@ -0,0 +1,33 @@
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { table } from 'console';
+import { get } from 'http';
+import { Observable, skip, take } from 'rxjs';
+import { association_card } from '../models/association.interface';
+import { AssociationService } from '../services/association.service';
+
+@Controller('association')
+export class AssociationController {
+ constructor( private  associationService:AssociationService){}
+    @Post()
+    create(@Body() card: association_card): Observable<association_card>
+    {
+      return this.associationService.createCard(card);
+    }
+
+    @Get()
+    findAll():Observable<association_card[]>{
+        return this.associationService.findAllAssociations();
+    }
+
+    // @Get()
+    // findSelected(@Query('take') take : number = 1,
+    // @Query('skip') skip : number = 1,
+    // ):Observable<association_card[]>{
+    //     take = take > 20 ? 20  : take;
+    //     return this.associationService.findCards(take,skip);
+    // }
+
+    
+
+
+}
---
+++ src/association/models/association.interface.ts
@@ -0,0 +1,11 @@
+export interface association_card{
+    id?:number;
+    nom?:string;
+    siege?:string;
+    objet?:string;
+    telephone?:number;
+    adresse?:string;
+    code_postal?:number;
+    ville?:string;
+    description?:string;
+}
