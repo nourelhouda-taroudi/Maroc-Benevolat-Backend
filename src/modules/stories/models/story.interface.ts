1aa090796442ec3b85bb75edbcc630f4e9f4ee67
fatihabardiche <bardichefatiha@gmail.com>
2022-05-21 15:53:55 +0100

    add changes

---
+++ src/modules/stories/models/story.interface.ts
@@ -0,0 +1,11 @@
+export interface Story{
+    id?:number;
+    text:string;
+    visualisation?:string;
+    image?:string;
+    like?:boolean;
+    commentaire?:string;
+    likeNum?:number;
+    createdAt?:Date;
+   
+}
