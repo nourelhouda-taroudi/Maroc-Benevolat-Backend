9b448e291040f20ea9a978d1bcbb91ce79937c58
Nourelhouda Taroudi <nourelhoudataroudi@gmail.com>
2022-05-21 20:19:49 +0100

    added validations :lipstick:

--- src/modules/user/dto/user-register.dto.ts
+++ src/modules/user/dto/user-register.dto.ts
@@ -1,9 +1,15 @@
+import { IsEmail, IsNotEmpty, IsNumber } from 'class-validator';
 import { AssociationDTO } from './../../association/dto/association.dto';
 export class UserSignUpDTO {
+  @IsNotEmpty()
   firstname: string;
+  @IsNotEmpty()
   lastname: string;
+  @IsNotEmpty()
+  @IsNumber()
   phone: number;
   gender: string;
+  @IsEmail()
   email: string;
   password: string;
   association: AssociationDTO;
--- src/modules/user/dto/user-signin.dto.ts
+++ src/modules/user/dto/user-signin.dto.ts
@@ -1,4 +1,9 @@
+import { IsEmail, IsEmpty, IsNotEmpty } from "class-validator";
+
 export class UserSignInDTO{
+    @IsEmail()
+    @IsNotEmpty()
     email: string;
+    @IsNotEmpty()
     password: string;
 }
