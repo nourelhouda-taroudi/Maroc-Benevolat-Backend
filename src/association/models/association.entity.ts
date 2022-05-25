8bed26a176294b49a6385d5847f6c2102037e477
oumaimamt <oumaimamoulaytaj@gmail.com>
2022-05-25 16:00:27 +0100

    updates

--- src/association/models/association.entity.ts
+++ src/association/models/association.entity.ts
@@ -2,7 +2,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 @Entity('association')
 export class AssociationEntity{
     @PrimaryGeneratedColumn()
-    id : number;
+  id: number;
     @Column()
     nom:string;
     @Column()
--- src/association/services/association.service.ts
+++ src/association/services/association.service.ts
@@ -1,13 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Param } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { format } from 'path';
-import { from, Observable } from 'rxjs';
-import { Repository } from 'typeorm';
+import { from, identity, Observable } from 'rxjs';
+import { Repository, UpdateResult } from 'typeorm';
 import { AssociationEntity } from '../models/association.entity';
 import { association_card } from '../models/association.interface';
 
 @Injectable()
 export class AssociationService {
+    public associations!: association_card[] ;
+   
+
+    
+;
 constructor(
     @InjectRepository(AssociationEntity)
     private readonly associationRepository: Repository<AssociationEntity>
@@ -21,13 +26,22 @@ findAllAssociations():Observable<association_card[]>{
     return from(this.associationRepository.find());
 }
 
-// findCards(take : number = 10, skip: number = 0):Observable<association_card[]>{
-//    return from(
-//     this.associationRepository.findAndCount({take, skip}).then(([cards]) => {
-//         return <association_card[]>cards
- 
-//     })
-//    );
-  
-// }
+
+
+async findAsso(id: number) {
+    return await this.associationRepository.findOne({
+      select: ['id', 'nom','siege','objet','telephone','adresse','code_postal','ville','description','email','facebook','instagram','twitter'],
+      where: {
+        id,
+      },
+    });
+  }
+
+  updateAsso(id:number,association: association_card):Observable<UpdateResult>{
+    return from(this.associationRepository.update(id,association));
+
 }
+
+
+}
+
